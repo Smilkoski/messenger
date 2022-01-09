b91641bc7981368f7a5f468a530d761713ec3578
Smilkoski <hristijan.smilkoski@gmail.com>
2022-01-10 00:54:41 +0100

    add Messenger,Chat

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,31 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+from users.models import CustomUser
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=50, default='')
+
+    def serializable(self):
+        return {'id': self.id,
+                'name': self.name,
+                'description': self.description, }
+
+
+class Message(models.Model):
+    message = models.CharField(max_length=140)
+    date = models.DateTimeField(default=datetime.now)
+    custom_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='message_custom_user')
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='message_group')
+
+    def serializable(self):
+        return {'id': self.id,
+                'message': self.message,
+                'date': self.date,
+                'custom_user': self.custom_user.user.username,
+                'custom_user_id': self.custom_user.id,
+                'custom_user_image_url': self.custom_user.image.url,
+                'group': self.group.serializable(), }
