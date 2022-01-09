6f8c95259a2fab53c4d5ea641da4b157703705a5
Smilkoski <hristijan.smilkoski@gmail.com>
2022-01-09 02:35:03 +0100

    add login, logout, register

--- users/views.py
+++ users/views.py
@@ -1,3 +1,61 @@
+from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = User.objects.get(username=username)
+
+        # Check if authentication successful
+        if user is not None and user.password == password:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "users/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        image = request.POST['img']
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "users/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User(username=username, email=email, password=password)
+            user.save()
+            cu = CustomUser(user=user, image=image)
+            cu.save()
+        except IntegrityError:
+            return render(request, "users/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "users/register.html")
